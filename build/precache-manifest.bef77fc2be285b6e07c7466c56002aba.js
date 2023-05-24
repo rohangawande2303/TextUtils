471657fb6aa1cacca615ee911e2fedf595607c21
Rohan <rohangawande2303@gmail.com>
2023-05-24 16:27:19 +0530

    Initial commit

---
+++ build/precache-manifest.bef77fc2be285b6e07c7466c56002aba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d42ea09419c08c971183",
+    "url": "/Textutils/static/css/main.adeef0c6.chunk.css"
+  },
+  {
+    "revision": "d42ea09419c08c971183",
+    "url": "/Textutils/static/js/main.d74c5f50.chunk.js"
+  },
+  {
+    "revision": "b5305c03e5a04519dd92",
+    "url": "/Textutils/static/js/runtime~main.a6dbe6f9.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/Textutils/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/Textutils/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "7820ea30c5a087ada2640a15ad250104",
+    "url": "/Textutils/index.html"
+  }
+];
